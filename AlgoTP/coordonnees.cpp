e4e76d2d35b110355a7aabace1101b7ca0958c18
Alexandre Trudeau <alextrudeausi291@gmail.com>
2016-09-27 21:18:46 -0400

    debut lirefichier

--- AlgoTP/coordonnees.cpp
+++ AlgoTP/coordonnees.cpp
@@ -30,23 +30,23 @@ void Coordonnees::setLongitude(double p_longitude) {
 	m_longitude = p_longitude;
 }
 
-static bool Coordonnees::is_valide_coord(double p_latitude, double p_longitude) {
-	if (p_latitude >= 0 && p_latitude <= 90 && p_longitude >= -180 && p_longitude <= 180)
-		return true;
-	else
-		return false;
-}
+//static bool Coordonnees::is_valide_coord(double p_latitude, double p_longitude) {
+//	if (p_latitude >= 0 && p_latitude <= 90 && p_longitude >= -180 && p_longitude <= 180)
+//		return true;
+//	else
+//		return false;
+//}
 
-double Coordonnees::operator- (const Coordonnees & p_other) const {
-	double d_lat = abs(this->getLatitude() - p_other.getLatitude());
-	double d_lon = abs(this->getLongitude() - p_other.getLongitude());
-	double a = asin(sqrt(pow(sin(d_lat / 2), 2) + cos(this->getLatitude()) * cos(p_other.getLatitude()) * pow(sin(d_lon / 2), 2)));
-	return 2 * earth_radius_km * a;
-}
+//double Coordonnees::operator- (const Coordonnees & p_other) const {
+//	double d_lat = abs(this->getLatitude() - p_other.getLatitude());
+//	double d_lon = abs(this->getLongitude() - p_other.getLongitude());
+//	double a = asin(sqrt(pow(sin(d_lat / 2), 2) + cos(this->getLatitude()) * cos(p_other.getLatitude()) * pow(sin(d_lon / 2), 2)));
+//	return 2 * earth_radius_km * a;
+//}
 
-friend std::ostream & Coordonnees::operator<<(std::ostream & p_flux, const Coordonnees & p_coord){
+//friend std::ostream & Coordonnees::operator<<(std::ostream & p_flux, const Coordonnees & p_coord){
 	// QU'EST-CE QUI FAUT AFFICHER???
-}
+//}
 
 double Coordonnees::degre_to_radian(double p_deg){
 	return (p_deg * M_PI / 180.0);
