6de92112619882c8b3dad1a540db308e0e6fe660
etudiant <etudiant@etudiant-VM>
2016-10-03 01:49:13 -0400

    Date et heure

--- AlgoTP/auxiliaires.cpp
+++ AlgoTP/auxiliaires.cpp
@@ -10,7 +10,9 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <ctime>
 #include "auxiliaires.h"
+#include "ContratException.h"
 
 using namespace std;
 
@@ -54,35 +56,113 @@ void lireFichier(std::string nomFichier,
 }
 
 Date::Date() {
+	time_t t;
+	time(&t);
+	tm *ltm = localtime(&t);
+	m_an = ltm->tm_year;
+	m_jour = ltm->tm_mday;
+	m_mois = ltm->tm_mon;
 }
-Date::Date(unsigned int an, unsigned int mois, unsigned int jour) {
+Date::Date(unsigned int an, unsigned int mois, unsigned int jour):
+	m_an(an), m_mois(mois), m_jour(jour){
+	long JourParMois[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	if(mois == 2)
+		if((an % 4 == 0 && an % 100 != 0) || (an % 4 == 0 && an % 100 == 0 && an % 400 ==0))
+			JourParMois[1]++;
+
+	PRECONDITION(mois > 0 && mois <= 12);
+	PRECONDITION(an > 1970 && an < 2037);
+	PRECONDITION(jour > 0 && jour <= JourParMois[mois-1]);
 }
 bool Date::operator==(const Date & other) const {
+	return (this->m_an == other.m_an && this->m_mois == other.m_mois && this->m_jour == other.m_jour)
 }
 bool Date::operator<(const Date & other) const {
+	if(this->m_an < other.m_an)
+		return true;
+	if(this->m_an == other.m_an){
+		if(this->m_mois < other.m_mois)
+			return true;
+		else{
+			if(this->m_mois == other.m_mois)
+				if(this->m_jour < other.m_jour)
+					return true;
+		}
+	}
+	return false;
 }
 bool Date::operator>(const Date & other) const {
+	if(this->m_an > other.m_an)
+		return true;
+	if(this->m_an == other.m_an){
+		if(this->m_mois > other.m_mois)
+			return true;
+		else{
+			if(this->m_mois == other.m_mois)
+				if(this->m_jour > other.m_jour)
+					return true;
+		}
+	}
+	return false;
 }
 std::ostream & operator<<(std::ostream & flux, const Date & p_date) {
+	flux << p_date.m_an << "-" << p_date.m_mois << "-" << p_date.m_jour;
+	return flux;
 }
 
 Heure::Heure() {
+	time_t t;
+	time(&t);
+	tm *ltm = localtime(&t);
+	m_heure = ltm->tm_hour;
+	m_min = ltm->tm_min;
+	m_sec = ltm->tm_sec;
 }
-Heure::Heure(unsigned int heure, unsigned int min, unsigned int sec) {
+Heure::Heure(unsigned int heure, unsigned int min, unsigned int sec) :
+		m_heure(heure), m_min(min), m_sec(sec) {
+	PRECONDITION(heure >= 0 && heure < 30);
+	PRECONDITION(min >= 0 && min < 60);
+	PRECONDITION(sec >= 0 && sec < 60);
 }
 Heure Heure::add_secondes(unsigned int secs) const {
+	unsigned int sec = m_sec + secs;
+	unsigned int min = m_min;
+	unsigned int hr = m_heure;
+	if (sec >= 60) {
+		sec -= 60;
+		min++;
+	}
+	if (min >= 60) {
+		min -= 60;
+		hr++;
+	}
+	return Heure(hr, min, sec);
 }
 bool Heure::operator==(const Heure & other) const {
+	return (this->m_heure == other.m_heure && this->m_min == other.m_min
+			&& this->m_sec == other.m_sec);
 }
 bool Heure::operator<(const Heure & other) const {
+	return (this->m_heure * 3600 + this->m_min * 60 + this->m_sec
+			< other.m_heure * 3600 + other.m_min * 60 + other.m_sec);
 }
 bool Heure::operator>(const Heure & other) const {
+	return (this->m_heure * 3600 + this->m_min * 60 + this->m_sec
+			> other.m_heure * 3600 + other.m_min * 60 + other.m_sec);
 }
 bool Heure::operator<=(const Heure & other) const {
+	return (this->operator==(other) || this->operator<(other));
 }
 bool Heure::operator>=(const Heure & other) const {
+	return (this->operator==(other) || this->operator>(other));
 }
-int Heure::operator-(const Heure & other) const {
+int Heure::operator-(const Heure & other) const {	//retour en secondes
+	return (this->m_heure * 3600 + this->m_min * 60 + this->m_sec)
+			- (other.m_heure * 3600 + other.m_min * 60 + other.m_sec);
 }
 std::ostream & operator<<(std::ostream & flux, const Heure & p_heure) {
+	flux << p_heure.m_heure << ":" << p_heure.m_min << ":" << p_heure.m_sec
+			<< endl;
+	return flux;
 }
--- AlgoTP/main.cpp
+++ AlgoTP/main.cpp
@@ -10,10 +10,10 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include "ContratException.h"
 #include "auxiliaires.h"
 #include "coordonnees.h"
 #include "arret.h"
-
 #include "station.h"
 #include "voyage.h"
 #include "ligne.h"
@@ -39,6 +39,11 @@ int main(){
 
 	cout << v_stations.size() << " Stations" << endl;
 	cout << v_lignes.size() << " Lignes" << endl;
+
+	Heure h = Heure(2,25,34);
+	Heure h1 = Heure(25,25,35);
+
+	cout << h1 - h << endl;
 
 	return 0;
 }
