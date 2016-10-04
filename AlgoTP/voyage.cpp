81da951593435369afd7e0e624d677c2b8874f42
Alexandre Trudeau <alextrudeausi291@gmail.com>
2016-10-03 23:13:41 -0400

    essaie d'une shit dans arretDeLaStation + fix de quelques warning

--- AlgoTP/voyage.cpp
+++ AlgoTP/voyage.cpp
@@ -16,7 +16,15 @@ Voyage::Voyage(const std::vector<std::string>& p_l, Ligne* p_ligne) :
 		m_id(p_l[2]), m_destination(p_l[3]), m_service_id(p_l[1]), m_ligne(p_ligne) {
 }
 Arret & Voyage::arretDeLaStation(unsigned int p_num_station) {
-	// Pas sure
+	for (unsigned int i = 0; i < m_arrets.size(); i++){
+		if( m_arrets[i].getStationId() == p_num_station) {
+			return m_arrets[i];
+		}
+
+		else {
+			// la station est pas dans le voyage
+		}
+	}
 }
 std::vector<Arret> Voyage::getArrets() const {
 	return m_arrets;
@@ -52,8 +60,8 @@ Heure Voyage::getHeureFin() const {
 	return m_arrets.back().getHeureArrivee();
 }
 void Voyage::setArrets(std::vector<Arret>& resultat) {
-	for (int i = 0; i < resultat.size(); i++) {
-		for (int j = 0; j < resultat.size(); j++){
+	for (unsigned int i = 0; i < resultat.size(); i++) {
+		for (unsigned int j = 0; j < resultat.size(); j++){
 			if(resultat[i] < resultat[j]){
 				Arret tmp = resultat[i];
 				resultat[i] = resultat[j];
@@ -69,5 +77,5 @@ bool Voyage::operator>(const Voyage & p_other) const {
 	return (this->getHeureDepart() > p_other.getHeureDepart());
 }
 std::ostream & operator<<(std::ostream & flux, const Voyage & p_voyage) {
-	flux << p_voyage.getLigne()->getNumero() << ": Vers " << p_voyage.getDestination() << endl;
+	return flux << p_voyage.getLigne()->getNumero() << ": Vers " << p_voyage.getDestination() << endl;
 }
