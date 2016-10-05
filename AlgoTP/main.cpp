36b59186bf9ea14ebbdd50c8a0272b91c892e834
etudiant <etudiant@etudiant-VM>
2016-10-05 19:51:41 -0400

    changement de repertoire fichiers txt + comment du main

--- AlgoTP/main.cpp
+++ AlgoTP/main.cpp
@@ -85,32 +85,39 @@ int main() {
 	map<string,vector<Arret>> v_arrets;
 	map<string,vector<Date>> v_datesVoyage;
 
-	importer("TXT/stops.txt", v_stations, true);
-	sort(v_stations.begin(),v_stations.end(),triStationId);
+	// Importation des stations du fichier stops.txt
+	importer("stops.txt", v_stations, true);
+	sort(v_stations.begin(),v_stations.end(),triStationId);	// Tri des stations selon id
 
-	importer("TXT/routes.txt", v_lignes, true);
-	sort(v_lignes.begin(),v_lignes.end(),triLigneNumero);
+	// Importation des lignes du fichier routes.txt
+	importer("routes.txt", v_lignes, true);
+	sort(v_lignes.begin(),v_lignes.end(),triLigneNumero);	// Tri des lignes selon numero
 
-	importerVoyages("TXT/trips.txt", v_voyages, v_lignes, true);
-	sort(v_voyages.begin(),v_voyages.end(),triVoyageId);
-	for (unsigned int i = 0; i < v_voyages.size(); i++){
+	// Importation des voyages du fichier trips.txt
+	importerVoyages("trips.txt", v_voyages, v_lignes, true);
+	sort(v_voyages.begin(),v_voyages.end(),triVoyageId);	// Tri des voyages selon id
+	for (unsigned int i = 0; i < v_voyages.size(); i++){	// Ajout des voyages dans les lignes
 		v_voyages[i].getLigne()->addVoyage(&v_voyages[i]);
 	}
 
-	importerArrets("TXT/stop_times.txt", v_arrets, true);
-	for (unsigned int a = 0; a < v_voyages.size(); a++){
+	// Importation des arrets du fichier stop_times.txt
+	importerArrets("stop_times.txt", v_arrets, true);
+	for (unsigned int a = 0; a < v_voyages.size(); a++){	// Modification des arrets des voyages
 		if(v_arrets.count(v_voyages[a].getId()))
 			v_voyages[a].setArrets(v_arrets[v_voyages[a].getId()]);
 	}
 
-	importerDates("TXT/calendar_dates.txt", v_datesVoyage, true);
+	// Importation des dates du fichier calendar_dates.txt
+	importerDates("calendar_dates.txt", v_datesVoyage, true);
 
+	// Affichage du temps d'exécution du chargement des données
 	cout << "Chargement des données terminé en " << Heure()-start << " secondes" << endl;
 
+	// Ouverture d'un fichier texte pour écriture
 	ofstream ecriture;
 	ecriture.open("FichierSortie.txt");
 
-	// ### Ecriture des lignes ordre numero ### ok
+	// Ecriture des lignes par ordre de leur numero
 	ecriture << "==================================" << endl;
 	ecriture << "LIGNES DE LA RTC" << endl;
 	ecriture << "COMPTE = " << v_lignes.size() << endl;
@@ -119,7 +126,7 @@ int main() {
 		ecriture << v_lignes[l] << endl;
 	}
 
-	// ### Ecriture des stations ordre numero ### ok
+	// Ecriture des stations par ordre de leur numero
 	ecriture << "==================================" << endl;
 	ecriture << "STATIONS DE LA RTC" << endl;
 	ecriture << "COMPTE = " << v_stations.size() << endl;
@@ -128,34 +135,38 @@ int main() {
 		ecriture << v_stations[i] << endl;
 	}
 
-	// ### Ecriture des voyages de la journée ### ok
+	// Definition des variables de la date et heure actuelle
 	Date currentDate = Date();
 	Heure now = Heure();
+	// Definition d'une variable pour l'heure future dans une heure
 	Heure inOneHour = now.add_secondes(3600);
+	// Definition d'un vecteur de voyages opérationnels au courant de la journée
+	// et dans la prochaine heure
 	vector<Voyage*> voyages2d;
+	// Ecriture des voyages de la journée et dans l'heure actuelle
+	// en ordre d'heure de départ
 	ecriture << "==================================" << endl;
 	ecriture << "VOYAGES DE LA JOURNÉE DU " << currentDate << endl;
 	ecriture << now << " - " << inOneHour << endl;
 	ecriture << "COMPTE = " << v_stations.size() << endl;
 	ecriture << "=====================================" << endl;
-
 	for (unsigned int v = 0; v < v_voyages.size(); v++){
-		vector<Date> dt = v_datesVoyage[v_voyages[v].getServiceId()];
-		vector<Date>::iterator it = find(dt.begin(), dt.end(), currentDate);
-		if(v_voyages[v].getHeureDepart() > now &&
-				v_voyages[v].getHeureFin() < inOneHour &&
-				it != dt.end()){
-			voyages2d.push_back(&v_voyages[v]);
+		vector<Date> dt = v_datesVoyage[v_voyages[v].getServiceId()];	// Extraire les dates opérationnels du service_id
+		vector<Date>::iterator it = find(dt.begin(), dt.end(), currentDate); // Trouver la date courrante dans les dates de service_id
+		if(v_voyages[v].getHeureDepart() > now &&			// Si le départ apres heure actuelle
+				v_voyages[v].getHeureFin() < inOneHour &&	// Si la fin avant dans une heure
+				it != dt.end()){							// Si le voyage est opérationnel aujourd'hui
+			voyages2d.push_back(&v_voyages[v]);				// Ajout dans le vecteur de voyage opérationnel
 		}
 	}
-	sort(voyages2d.begin(),voyages2d.end(), triHeureDep);
+	sort(voyages2d.begin(),voyages2d.end(), triHeureDep);	// Tri du vecteur de voyages opérationnels selon l'heure de depart
 	for(unsigned int vt = 0; vt < voyages2d.size(); vt++){
-		ecriture << (*voyages2d[vt]) << endl;
+		ecriture << (*voyages2d[vt]) << endl;				// Ecrire le voyage
 		for (unsigned int a = 0; a < voyages2d[vt]->getArrets().size(); a++){
-			ecriture << voyages2d[vt]->getArrets()[a] << endl;
+			ecriture << voyages2d[vt]->getArrets()[a] << endl;	// Ecrire les arrets du voyage
 		}
 	}
 
-	ecriture.close();
+	ecriture.close();	// Fermer le fichier d'écriture
 	return 0;
 }
